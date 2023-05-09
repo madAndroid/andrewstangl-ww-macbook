e4b7df8396445f4f7ab4508ac00ffbdbe4367d4b
Paul Carlton <paul.carlton@weave.works>
2023-05-09 16:22:40 +0100

    updated gitopssets image

--- leafs/leaf-deploy.sh
+++ leafs/leaf-deploy.sh
@@ -41,7 +41,24 @@ sudo kubeadm reset -f
 sudo rm -rf /etc/cni/net.d
 sudo ipvsadm --clear
 
-sudo -E kubeadm init --ignore-preflight-errors=NumCPU --pod-network-cidr "10.0.0.0/8" --kubernetes-version v1.27.0 --apiserver-cert-extra-sans $hostname
+sudo systemctl stop firewalld.service 
+sudo systemctl disable firewalld.service
+
+sudo systemctl stop kubelet
+sudo systemctl stop docker
+sudo iptables --flush
+sudo iptables -tnat --flush
+sudo systemctl start kubelet
+sudo systemctl start docker
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -F
+
+sudo systemctl restart containerd
+
+sleep 5
+sudo -E kubeadm init --ignore-preflight-errors=NumCPU --pod-network-cidr "10.10.0.0/16"  --service-cidr "10.11.0.0/16" --kubernetes-version v1.27.0 --apiserver-cert-extra-sans $hostname
 
 mkdir -p $HOME/.kube
 sudo cp -rf /etc/kubernetes/admin.conf $HOME/.kube/config
