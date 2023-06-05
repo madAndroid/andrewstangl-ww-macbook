aea4fa387c98088450ed9a3b7c52dc5e017d8ac9
Paul Carlton <paul.carlton@weave.works>
2023-06-05 13:44:05 +0100

    add test-two

--- bin/deploy-resource.sh
+++ bin/deploy-resource.sh
@@ -66,7 +66,7 @@ source .envrc
 
 export nameSpace=$namespace
 cat cluster/namespace/$template_name.yaml| envsubst > /tmp/$template_name.yaml
-gitops create template /tmp/$template_name.yaml --values RESOURCE_NAME=$resource_name AWS_REGION=$AWS_REGION --output-dir .;git add -A ; git commit -a -m "create resource $resource_name";git pull;git push
+gitops create template /tmp/$template_name.yaml --values RESOURCE_NAME=$resource_name AWS_REGION=$AWS_REGION --output-dir .
 
 git add clusters/management/$namespace/$resource_name.yaml
 if [[ `git status --porcelain` ]]; then
