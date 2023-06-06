00430a3ea89e9bd604e69e9233a4533e506a7389
Paul Carlton <paul.carlton@weave.works>
2023-06-06 15:45:47 +0100

    update template scripts

--- bin/remove-resource.sh
+++ bin/remove-resource.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env bash
 
-# Utility creating resources
+# Utility deleting resources
 # Version: 1.0
 # Author: Paul Carlton (mailto:paul.carlton@weave.works)
 
@@ -9,17 +9,13 @@ set -euo pipefail
 
 function usage()
 {
-    echo "usage ${0} [--debug] --env-template <environment-template> --template-name <template-name> --namespace <namespace> --resource-name <resource-name>" >&2
-    echo "This script will create resources" >&2
-    echo " The --env-template option is used to specify the environment template" >&2
-    echo " The --template-name option is used to specify the template name" >&2
+    echo "usage ${0} [--debug] --namespace <namespace> --resource-name <resource-name>" >&2
+    echo "This script will delete resources" >&2
     echo " The --namespace option is used to specify the namespace" >&2
     echo " The --resource-name option is used to specify the resource name" >&2
 }
 
 function args() {
-  env_template
-  template_name=""
   resource_name=""
   namespace=""
 
@@ -28,8 +24,6 @@ function args() {
   arg_index=0
   while (( arg_index < arg_count )); do
     case "${arg_list[${arg_index}]}" in
-          "--env-template") (( arg_index+=1 ));env_template=${arg_list[${arg_index}]};;
-          "--template-name") (( arg_index+=1 ));template_name=${arg_list[${arg_index}]};;
           "--namespace") (( arg_index+=1 ));namespace=${arg_list[${arg_index}]};;
           "--resource-name") (( arg_index+=1 ));resource_name=${arg_list[${arg_index}]};;
           "--debug") set -x;;
@@ -44,16 +38,6 @@ function args() {
     esac
     (( arg_index+=1 ))
   done
-
-  if [ -z "$env_template" ]; then
-    echo "missing --env-template option" >&2
-    usage; exit
-  fi
-
-  if [ -z "$template_name" ]; then
-    echo "missing --template-name option" >&2
-    usage; exit
-  fi
 
   if [ -z "$namespace" ]; then
     echo "missing --namespace option" >&2
@@ -72,16 +56,13 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR/.. >/dev/null
 source .envrc
 
-export nameSpace=$namespace
-cat cluster/namespace/$template_name.yaml | envsubst > /tmp/$template_name.yaml
-gitops create template /tmp/$template_name.yaml --values RESOURCE_NAME=$resource_name AWS_REGION=$AWS_REGION --output-dir .
+rm clusters/management/clusters/$namespace/$resource_name.yaml
 git add clusters/management/clusters/$namespace/$resource_name.yaml
-
-cat resource-descriptions/templates/$env_template.yaml | envsubst > resource-descriptions/$namespace/$resource_name.yaml
+rm resource-descriptions/$namespace/$resource_name.yaml
 git add resource-descriptions/$namespace/$resource_name.yaml
 
 if [[ `git status --porcelain` ]]; then
-  git commit -m "deploy resource $resource_name using template $template_name in namespace $namespace"
+  git commit -m "remove resource $resource_name in namespace $namespace"
   git pull
   git push
 fi
