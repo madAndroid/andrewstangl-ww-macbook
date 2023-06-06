291f65364972ff476de749ec827acfeaf2148c26
Paul Carlton <paul.carlton@weave.works>
2023-06-06 15:35:16 +0100

    refactor template

--- terraform/cluster-templates/vpc-discover/main.tf
+++ terraform/cluster-templates/vpc-discover/main.tf
@@ -9,9 +9,6 @@ provider "aws" {
 }
 
 module "vpc" {
-  source               = "../../modules/vpc"
+  source               = "../../modules/vpc-discover"
   vpc_name             = var.vpc_name
-  vpc_cidr             = var.vpc_cidr
-  public_subnet_count  = var.public_subnet_count
-  private_subnet_count = var.private_subnet_count
 }
