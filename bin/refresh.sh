f5a2b77ed93308b55480a1bfadfe2e715c6f01a0
Paul Carlton <paul.carlton@weave.works>
2023-06-13 11:53:52 +0100

    add wge stuff

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -6,6 +6,10 @@
 
 . ww-aws.sh
 aws-secrets.sh
+
+export AWS_B64ENCODED_CREDENTIALS=$(clusterawsadm bootstrap credentials encode-as-profile)
+clusterctl init --infrastructure aws
+
 kubectl rollout restart deployment -n flux-system  source-controller 
 kubectl rollout restart deployment -n flux-system  kustomize-controller 
 kubectl rollout restart deployment -n flux-system  weave-gitops-enterprise-mccp-cluster-service
