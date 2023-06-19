51b39d8acf9cb6e23818f844e6ee3c9d203600a9
Paul Carlton <paul.carlton@weave.works>
2023-06-19 19:33:01 +0100

    kind wge

--- kind-leafs/leaf-deploy.sh
+++ kind-leafs/leaf-deploy.sh
@@ -50,7 +50,7 @@ if [ $ret -ne 0 ]; then
   cat /tmp/kind.yaml | envsubst > /tmp/kind-config.yaml
   sudo mkdir -p $HOME/kubernetes/log
   sudo mkdir -p $HOME/kubernetes/policies
-  sudo cp -rf /tmp/audit.yaml $HOME/kubernetes/policies/audit.yaml
+  cp -rf /tmp/audit.yaml $HOME/kubernetes/policies/audit.yaml
   kind create cluster --name ${cluster_name} --config /tmp/kind-config.yaml
 
   while [ 1 -eq 1 ]
